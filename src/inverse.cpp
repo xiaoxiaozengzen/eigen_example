69a0ee34c714b65956886aca194c86a89dd0db7f
chenguangzeng <chenguangzeng@cgz.com>
2026-05-13 18:07:27 +0800

    feat: add yingshe

--- src/inverse.cpp
+++ src/inverse.cpp
@@ -28,10 +28,18 @@ void inverse_stright(const Eigen::Matrix3d& r, const Eigen::Vector3d& t) {
  *      [ R^-1  -R^-1 * t ]
  *      [ 0     1          ]
  *  3.其中R^-1是R的逆矩阵，-R^-1 * t是R^-1与t的乘积的相反数。
+ * @note 也可以这样理解：
+ * 1. Xc = R * Xw + t, 世界坐标系的点，转成相机坐标系的点
+ * 2. 反过来，相机坐标系转世界坐标系就需要求逆：
+ *    Xw = R^-1 * (Xc - t) = R^-1 * Xc - R^-1 * t
+ *    其中R^-1是R的逆矩阵，-R^-1 * t是R^-1与t的乘积的相反数。
+ * 3. 因此，分块矩阵求逆的结果就是上述的形式。
  */
 void inverse_block(const Eigen::Matrix3d& r, const Eigen::Vector3d& t) {
     Eigen::Matrix4d ext = Eigen::Matrix4d::Identity();
+    // 1.求R的逆矩阵
     Eigen::Matrix3d r_inv = r.inverse();
+    // 2.求-R^-1 * t
     ext.block<3, 3>(0, 0) = r_inv;
     Eigen::Vector3d t_inv = r_inv * t;
     ext(0, 3) = t_inv(0) * -1;
